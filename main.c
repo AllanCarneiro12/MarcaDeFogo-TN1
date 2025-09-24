bce951ee9a860d0f65303e7e21245ee3370b7e2f
Isadora <isadoravoss2007@gmail.com>
2025-09-24 11:43:33 -0300

    Tentativa de rodizio

--- main.c
+++ main.c
@@ -300,6 +300,61 @@ int main()
                 printf("==========={%7s}===========\n", carreta.placa);
             }
         }
+        else if(menu == 2) //para o rodizio de pneus
+        {
+           int pneu1, pneu2;//valores de pneus a serem trocados
+           int i1=-1, j1=-1, i2=-1, j2 = -1 //o -1 verifica se os valores exitem na matriz
+         if (eixo == 1) // Se for truck ele entra nesse if
+            {
+                struct Truck trucado;
+                printf("\nDigite o primeiro pneu a trocar: ");
+                scanf("%d", &pneu1);
+                printf("Digite o segundo pneu a trocar: ");
+                scanf("%d", &pneu2);
+
+                //Pegar as posicoes dos pneus
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++) 
+                    {
+                        if(trucado.pneus[i][j]==pneu1)
+                        {
+                            i1 = i;
+                            j1 = j;
+                        }
+                        if(trucado.pneus[i][j]==pneu2)
+                        {
+                            i2 = i;
+                            j2 = j;
+                        }
+                       
+                    }
+                    printf("\n");
+                }
+                //verifica se as posicoes existem e troca de lugar
+                if(i1 != -1 && i2 != -1)
+                {
+                    int temp = trucado.pneus[i1][j1]; 
+                  trucado.pneus[i1][j1] = trucado.pneus[i2][j2];
+                  trucado.pneus[i2][j2] = temp;
+                
+                //mostra os pneus após a troca
+                printf("Mostrar pneus após a troca:\n");
+                for(i=0; i<3; i++)
+                {
+                    for(j=0; j<4; j++)
+                    {
+                        printf("%d\t",trucado.pneus[i][j]);
+                    }
+                    printf("\n");
+                }
+            }
+            else{
+                printf("Pneus não encontrados!");
+            }
+            }
+        } 
+
 
     } while (menu != 0);
 
